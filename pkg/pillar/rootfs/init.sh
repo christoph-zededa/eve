b7994666fc652fc2d6de156b16f885e184386b45
Zededa Guest <zededa@Zededas-MacBook-Pro.local>
2023-01-09 11:52:50 +0100

    config

--- pkg/pillar/rootfs/init.sh
+++ pkg/pillar/rootfs/init.sh
@@ -5,6 +5,9 @@
 # Start with a default content for resolv.conf
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 
+echo '172.16.18.242 zedcontrol.local.zededa.net' >> /etc/hosts
+echo '172.16.18.242 zedcloud.local.zededa.net' >> /etc/hosts
+
 #Copy pre-defined fscrypt.conf
 cp fscrypt.conf /etc/fscrypt.conf
 
