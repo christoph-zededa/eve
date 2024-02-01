2850c14d39a9d69d50e217f850c1fa846792fac8
Christoph Ostarek <christoph@zededa.com>
2024-02-01 22:37:15 +0100

    Dockerfile.ubuntu

---
+++ pkg/pillar/init_ubuntu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+sudo mkdir -p /sys/fs/cgroup/init
+echo 1 | sudo tee /sys/fs/cgroup/init/cgroup.procs
+sudo echo +cpu > /sys/fs/cgroup/cgroup.subtree_control
+
+cd /tmp
+#sudo nohup /usr/local/bin/containerd &
+
+tmux new-session -A -s containerd\; send -t containerd "sudo /usr/local/bin/containerd" ENTER \; detach -s containerd
+sleep 5
+sudo chmod a+rw /run/containerd/containerd.sock
+
+cd /src
+
+exec /bin/bash
